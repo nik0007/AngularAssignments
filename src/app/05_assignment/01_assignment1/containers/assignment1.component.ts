b8c1fe17ca9d96bc2d5a43fd6666d24215051bf6
Nikhil Bains <nikbains7@gmail.com>
2020-04-22 16:17:28 +0530

    Angular

--- src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
+++ src/app/05_assignment/01_assignment1/containers/assignment1.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-assignment1',
   templateUrl: './assignment1.component.html',
-  styleUrls: ['./assignment1.component.css']
+  styleUrls: ['./assignment1.component.css'],
 })
-export class Assignment1Component {
 
+export class Assignment1Component {
+  textMessage: string = ''
+  handler1(value: string): void{
+    this.textMessage = value
+  } 
 }
